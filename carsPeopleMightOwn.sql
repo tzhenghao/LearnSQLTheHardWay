b22f066b1aee6f321e07b6ab950b6807e45301f5
Zheng Hao Tan <tanzhao@umich.edu>
2014-05-13 13:58:54 +0800

    Added extra credit exercise

---
+++ carsPeopleMightOwn.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Person (
+	personID INTEGER,
+	age INTEGER
+);
+
+CREATE TABLE PersonCar (
+	personID INTEGER,
+	carID INTEGER
+);
+
+CREATE TABLE Car (
+	carID INTEGER,
+	color TEXT,
+	year INTEGER
+);

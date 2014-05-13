8963c5c88e7c724563920a1f7eafde70250f0cbd
Zheng Hao Tan <tanzhao@umich.edu>
2014-05-13 17:05:20 +0800

    Completed Exercise 3

--- exercise.sql
+++ exercise.sql
@@ -17,3 +17,11 @@ CREATE TABLE PersonPet (
 	personID INTEGER,
 	petID INTEGER
 );
+
+INSERT INTO Person (id, firstName, lastName, age)
+	VALUES	(0, "Zed", "Shaw", 37);
+
+INSERT INTO Pet (id, name, breed, age, dead)
+	VALUES (0, "Fluffy", "Unicorn", 1000, 0);
+
+INSERT INTO Pet VALUES (1, "Gigantor", "Robot", 1, 1);
